305ee4e7f84615ce41580ce04ebc9aa188bd7150
danmkt00 <dani4ku@gmail.com>
2025-05-06 10:42:20 +0200

    add promises exercises

--- 03-promises/exercises/even-number.js
+++ 03-promises/exercises/even-number.js
@@ -18,42 +18,46 @@ const { log, error } = labeledLogger();
 
 // declare callbacks
 const logSuccessFailure = (resolvedValue) => {
-    const finalMessage =
-        resolvedValue === true ? 'number is even' : 'number is not even';
-    log(finalMessage);
+  const finalMessage =
+    resolvedValue === true ? 'number is even' : 'number is not even';
+  log(finalMessage);
 };
-const mustBeGreaterThanZero = (resolvedValue) => {
-    log('checking if ' + resolvedValue + ' is greater than 0');
-    if (resolvedValue <= 0) {
-        throw new RangeError(resolvedValue + ' is not greater than 0');
-    }
-    return resolvedValue;
-};
-const inputANumberExecutor = (resolve, reject) => {
-    const userInput = prompt('enter an even number greater than 0');
-    const userNumber = Number(userInput);
-    const isANumber =
-        userInput !== '' && userInput !== null && !Number.isNaN(userNumber);
 
-    if (_) {
-        _(_);
-    } else {
-        _('input is not a number: ' + userInput);
-    }
+const mustBeGreaterThanZero = (resolvedValue) => {
+  log('checking if ' + resolvedValue + ' is greater than 0');
+  if (resolvedValue <= 0) {
+    throw new RangeError(resolvedValue + ' is not greater than 0');
+  }
+  return resolvedValue;
 };
+
+const inputANumberExecutor = (resolve, reject) => {
+  const userInput = prompt('enter an even number greater than 0');
+  const userNumber = Number(userInput);
+  const isANumber =
+    userInput !== '' && userInput !== null && !Number.isNaN(userNumber);
+
+  if (isANumber) {
+    resolve('input is a number: ' + userInput);
+  } else {
+    reject('input is not a number: ' + userInput);
+  }
+};
+
 const handleRejection = (err) => {
-    error('promise was rejected:', err);
+  error('promise was rejected:', err);
 };
+
 const isEvenNumber = (resolvedValue) => {
-    log('checking if ' + resolvedValue + ' is even');
-    return resolvedValue % 2 === 0;
+  log('checking if ' + resolvedValue + ' is even');
+  return resolvedValue % 2 === 0;
 };
 
 // use callbacks
-new Promise(_)
-    ._(_) // is the number greater than zero?
-    ._(_) // is the number even?
-    ._(_) // log the success/failure of the user's input
-    ._(_); // did an error or rejection occur?
+new Promise(inputANumberExecutor)
+  .then(mustBeGreaterThanZero) // is the number greater than zero?
+  .then(isEvenNumber) // is the number even?
+  .then(logSuccessFailure) // log the success/failure of the user's input
+  .catch(handleRejection); // did an error or rejection occur?
 
 log('= = = =  the call stack is empty  = = = =');
