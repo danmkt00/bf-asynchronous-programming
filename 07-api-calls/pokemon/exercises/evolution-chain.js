848b4065684d8329d9433dc9206638bb48e590a8
danmkt00 <dani4ku@gmail.com>
2025-05-08 13:47:05 +0200

    add api-calls exercises

--- 07-api-calls/pokemon/exercises/evolution-chain.js
+++ 07-api-calls/pokemon/exercises/evolution-chain.js
@@ -10,30 +10,43 @@ import { ORIGIN } from '../config.js';
  * @throws {Error} HTTP error! status: {number}.
  */
 export const evolutionChain = async (chainId = 1) => {
-    // --- generate and declare your resource's URL ---
-    // docs: https://pokeapi.co/docs/v2#evolution-section
-    const URL = _;
+  // --- generate and declare your resource's URL ---
+  // docs: https://pokeapi.co/docs/v2#evolution-section
+  const URL = `https://pokeapi.co/api/v2/evolution-chain/${chainId}`;
+  console.log(URL);
 
-    // --- fetch the API data (this works!) ---
-    const encodedURL = encodeURI(URL);
-    const response = await fetch(encodedURL);
+  // --- fetch the API data (this works!) ---
+  const encodedURL = encodeURI(URL);
+  const response = await fetch(encodedURL);
 
-    // --- throw an error if the response is not ok (this works!) ---
-    if (!response.ok) {
-        const message = response.statusText
-            ? `${response.status}: ${response.statusText}\n-> ${URL}`
-            : `HTTP error! status: ${response.status}\n-> ${URL}`;
-        throw new Error(message);
-    }
+  // --- throw an error if the response is not ok (this works!) ---
+  if (!response.ok) {
+    const message = response.statusText
+      ? `${response.status}: ${response.statusText}\n-> ${URL}`
+      : `HTTP error! status: ${response.status}\n-> ${URL}`;
+    throw new Error(message);
+  }
 
-    /* --- parse the data if the response was ok (this works!) ---*/
-    const data = await response.json();
+  /* --- parse the data if the response was ok (this works!) ---*/
 
-    // --- process the fetched data (if necessary) ---
-    //  you do not need to use `await` below this comment
-    //  you can refactor this to a separate logic function and test it
-    _; // tricky one!  you will need to push all the species into an array
+  // --- process the fetched data (if necessary) ---
+  //  you do not need to use `await` below this comment
+  //  you can refactor this to a separate logic function and test it
+  // const pokemon = []; // tricky one!  you will need to push all the species into an array
+  const data = await response.json();
+  let current = data.chain;
 
-    // --- return the final data ---
-    return pokemon;
+  const pokemon = [];
+
+  pokemon.push(current.species);
+
+  while (current.evolves_to.length > 0) {
+    pokemon.push(current.evolves_to[0].species);
+    current = current.evolves_to[0];
+  }
+
+  console.log(pokemon);
+
+  // --- return the final data ---
+  return pokemon;
 };
